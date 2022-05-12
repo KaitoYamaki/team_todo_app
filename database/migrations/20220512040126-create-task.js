69604341b5ce411459ecd698005272d3aadbf575
KaitoYamaki <105027673+KaitoYamaki@users.noreply.github.com>
2022-05-12 13:05:26 +0900

    taskテーブルの作成

---
+++ database/migrations/20220512040126-create-task.js
@@ -0,0 +1,41 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Tasks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      teamId: {
+        allowNull:false,
+        type: Sequelize.INTEGER,
+        refereces: {model: 'Teams', key: 'id'}
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      body: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      status: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Tasks');
+  }
+};
