84725c8c9cabaaea1fe159b91855059964366bf6
KaitoYamaki <105027673+KaitoYamaki@users.noreply.github.com>
2022-05-19 16:05:00 +0900

    commit

--- app/middlewares/managable_team.js
+++ app/middlewares/managable_team.js
@@ -1,7 +1,15 @@
-module.exports = async function forceAdmin(req, res, next) {
-    if (req.user.isAdmin()) {
-      return next();
-    }
+const models = require('../models');
+
+module.exports = async function managableTeam(req, res, next) {
+  const team = await models.Team.findByPk(req.params.team)
+  const manageMember = await team.getTeamMember({
+    where: { role: 1 }
+  });
+  const manageUser = manageMember[0];
+  if(manageUser.userId === req.user.id) {
+    return next();
+  }
+    else 
     await req.flash('alert', 'アクセスできません');
-    res.redirect('/');
-  };
+      res.redirect('/');
+}; 
