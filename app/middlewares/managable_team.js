2f862a00b239062782dd63881ddcf67f63cc427c
KaitoYamaki <105027673+KaitoYamaki@users.noreply.github.com>
2022-05-20 19:00:19 +0900

    修正

--- app/middlewares/managable_team.js
+++ app/middlewares/managable_team.js
@@ -3,13 +3,11 @@ const models = require('../models');
 module.exports = async function managableTeam(req, res, next) {
   const team = await models.Team.findByPk(req.params.team)
   const manageMember = await team.getTeamMember({
-    where: { role: 1 }
+    where: { role: 1, userId: req.user.id}
   });
-  const manageUser = manageMember[0];
-  if(manageUser.userId === req.user.id) {
+  if(manageMember.length>0) {
     return next();
   }
-    else 
     await req.flash('alert', 'アクセスできません');
       res.redirect('/');
 }; 
